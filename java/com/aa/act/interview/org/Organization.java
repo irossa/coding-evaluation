9566eb582c61eba918f699f6a1f6b661efb102cf
irossa <wcalroberts@hotmail.com>
2023-06-28 18:27:33 -0700

    changes to Organization file to update employee positions

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,6 +1,7 @@
 package com.aa.act.interview.org;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public abstract class Organization {
 
@@ -20,10 +21,35 @@ public abstract class Organization {
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
-        return Optional.empty();
+        
+    	return checkDirectReports(root, person, title);
+   
     }
 
+    private Optional<Position> checkDirectReports(Position pos, Name person, String title) {
+
+    	if (!pos.isFilled() && title == pos.getTitle()) {
+	    	Employee emp = new Employee(generateUniqueId(),person);
+	        pos.setEmployee(Optional.of(emp));
+	        return Optional.of(pos);
+    	} else {
+	    	for (Position p : pos.getDirectReports()) {
+	    		Optional<Position> filledPosition = checkDirectReports(p, person, title);
+	    		if (filledPosition.isPresent()) return filledPosition;
+	    	}
+    	}
+    	return Optional.empty();
+    }
+    
+    private static int generateUniqueId() {
+    	UUID idOne = UUID.randomUUID();
+        String str=""+idOne;        
+        int uid=str.hashCode();
+        String filterStr=""+uid;
+        str=filterStr.replaceAll("-", "");
+        return Integer.parseInt(str);
+    }
+    
     @Override
     public String toString() {
         return printOrganization(root, "");
